64bffd525ff5d4cd5e854dcfe6dc97f46923c926
alexandretampieri <50683220+alexandretampieri@users.noreply.github.com>
2019-05-16 17:00:24 -0300

    Novas linhas

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ echo "Meu primeiro código PHP! <strong> Hello World! </strong> <br>" ;
 
 echo "Meu primeiro código PHP! <b> Hello World! </b> <br>";
 
-echo "Olá Mundo!";
+echo "Olá Mundo Cruel!";
+
+echo "Olá Brasil!"
 
 ?>
